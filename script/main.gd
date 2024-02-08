0812db8078629350a4cc7dbf3da0da0c909f43a9
lm103841 <mattys.lemoine@etu.unice.fr>
2024-02-08 18:55:55 +0100

    parler npc1

--- script/main.gd
+++ script/main.gd
@@ -1,5 +1,6 @@
 extends Node
 
+signal new_team_mate()
 var team: int
 # Called when the node enters the scene tree for the first time.
 func _ready():
--- script/npc1.gd
+++ script/npc1.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+var talking = false
 
 const lines: Array[String] = [
 	"Bob : Yo !",
@@ -7,6 +8,25 @@ const lines: Array[String] = [
 	"Attend moi j'arrive"
 ]
 
-func _unhandled_input(event):
-	if event.is_action_pressed("click"):
+#func _unhandled_input(event):
+#	if event.is_action_pressed("click"):
+#		DialogManager.start_dialog(global_position, lines)
+
+
+#func _input(event):
+	#if event.is_action_pressed("adc"):
+
+		#if get_viewport_rect().has_point(to_local(event.position)):
+
+			#DialogManager.start_dialog(global_position, lines)
+
+
+
+func _on_area_2d_body_entered(body):
+	if !talking:
 		DialogManager.start_dialog(global_position, lines)
+		talking = true
+
+
+func _on_area_2d_body_exited(body):
+	talking = false
