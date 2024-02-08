bfa6ed4717a216143bd38d3f90ff40094cf69bd5
lm103841 <mattys.lemoine@etu.unice.fr>
2024-02-08 18:04:09 +0100

    dialogue test

--- script/npc1.gd
+++ script/npc1.gd
@@ -1,1 +1,12 @@
 extends CharacterBody2D
+
+
+const lines: Array[String] = [
+	"Bob : Yo !",
+	"Ca joue ?",
+	"Attend moi j'arrive"
+]
+
+func _unhandled_input(event):
+	if event.is_action_pressed("click"):
+		DialogManager.start_dialog(global_position, lines)
--- script/player.gd
+++ script/player.gd
@@ -10,7 +10,7 @@ func _input(event):
 
 func _physics_process(delta):
 	velocity = position.direction_to(target) * speed
-	print(velocity)
+	#print(velocity)
 	if position.distance_to(target) > 10:
 		move_and_slide()
 	else:
