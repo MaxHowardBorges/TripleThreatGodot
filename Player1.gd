5b4e71259b032ee01c574686fe8219db910064c8
Mattys Lemoine <62544576+MattysLE@users.noreply.github.com>
2024-02-19 15:09:35 +0100

    idle ball

--- Player1.gd
+++ Player1.gd
@@ -9,6 +9,7 @@ var shooting = false
 var screen_size
 var target_clicked = position
 var key_states = {}
+var lastPlayedAnimation = ""
 @onready var target = $"../basketPoint"
 @onready var ray = $"RayCast2D"
 @onready var shootBar = $"../Control/ShootBar"
@@ -32,8 +33,8 @@ func _physics_process(delta):
 
 	else:
 		velocity = Vector2.ZERO
-		stop_moving_and_reset_animation()
-
+		#stop_moving_and_reset_animation()
+		
 	update_animation(velocity)
 	if hasBall:
 		if Input.is_action_pressed("espace") && !shooting:
@@ -46,10 +47,13 @@ func _physics_process(delta):
 				shootPressed.emit()
 				shooting = true
 		elif key_states.has("shoot") and key_states["shoot"]:
+			velocity = Vector2.ZERO
 			key_states["shoot"] = false
 			shootReleased.emit(position)
 			shooting = false
 			hasBall = false
+			$AnimatedSprite2D.animation = "shoot_up"
+			lastPlayedAnimation = "shoot_up"
 
 func stop_moving_and_reset_animation():
 	velocity = Vector2.ZERO
@@ -62,24 +66,42 @@ func update_animation(velocity):
 		if hasBall:
 			if velocity.x > 0 and velocity.x > abs(velocity.y):
 				$AnimatedSprite2D.animation = "right_b"
+				lastPlayedAnimation = "right_b"
 			elif velocity.x < 0 and abs(velocity.x) > abs(velocity.y):
 				$AnimatedSprite2D.animation = "left_b"
+				lastPlayedAnimation = "left_b"
 			elif velocity.y < 0 and abs(velocity.y) > abs(velocity.x):
 				$AnimatedSprite2D.animation = "up_b"
+				lastPlayedAnimation = "up_b"
 			elif velocity.y > 0 and abs(velocity.y) > abs(velocity.x):
 				$AnimatedSprite2D.animation = "down_b"
+				lastPlayedAnimation = "down_b"
 		else:
 			if velocity.x > 0 and velocity.x > abs(velocity.y):
 				$AnimatedSprite2D.animation = "right"
+				lastPlayedAnimation = "right"
 			elif velocity.x < 0 and abs(velocity.x) > abs(velocity.y):
 				$AnimatedSprite2D.animation = "left"
+				lastPlayedAnimation = "left"
 			elif velocity.y < 0 and abs(velocity.y) > abs(velocity.x):
 				$AnimatedSprite2D.animation = "up"
+				lastPlayedAnimation = "up"
 			elif velocity.y > 0 and abs(velocity.y) > abs(velocity.x):
 				$AnimatedSprite2D.animation = "down"
+				lastPlayedAnimation = "down"
 	else:
-		stop_moving_and_reset_animation()
-
+		#stop_moving_and_reset_animation()
+		if hasBall:
+			if lastPlayedAnimation == "up_b":
+				$AnimatedSprite2D.animation = "idle_b_up"
+			elif lastPlayedAnimation == "right_b":
+				$AnimatedSprite2D.animation = "idle_b_right"
+			elif lastPlayedAnimation == "left_b":
+				$AnimatedSprite2D.animation = "idle_b_left"
+			elif lastPlayedAnimation == "down_b":
+				$AnimatedSprite2D.animation = "idle_b_down"
+		else:
+			$AnimatedSprite2D.animation = "idle_up"
 
 func _on_ball_has_ball_player_1():
 	hasBall = true
